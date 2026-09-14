7b338fe896dc6b618875adfa825b19ce6ce4d22b
luislima9970 <pt.luis.lima2006@gmail.com>
2026-09-14 22:04:47 +0100

    Scrool tested and added

--- app/src/componets/Canvas.tsx
+++ app/src/componets/Canvas.tsx
@@ -2,27 +2,38 @@ import { useRef, useReducer } from "react";
 import { Camera } from "../../../automata-visualizer/src/Camera.js";
 
 function Canvas() {
-  const cameraRef = useRef(new Camera(0, 0, 1, 800, 600));
-  const [, forceRender] = useReducer(x => x + 1, 0);
+    const cameraRef = useRef(new Camera(0, 0, 1, 800, 600));
+    const [, forceRender] = useReducer(x => x + 1, 0);
 
-  function handleMouseMove(e: React.MouseEvent) {
-    if (e.buttons === 1) { // left mouse button is held down
-      cameraRef.current.pan(e.movementX, e.movementY);
-      forceRender();
+    function handleMouseMove(e: React.MouseEvent) {
+        if (e.buttons === 1) {
+            cameraRef.current.pan(e.movementX, e.movementY);
+            forceRender();
+        }
     }
-  }
 
-  return (
-    <svg
-      width={800}
-      height={600}
-      viewBox={cameraRef.current.viewBoxString}
-      style={{ border: "1px solid black" }}
-      onMouseMove={handleMouseMove}
-    >
-      <rect x={350} y={250} width={100} height={100} fill="steelblue" />
-    </svg>
-  );
+    function handleWheel(e: React.WheelEvent) {
+        const factor = e.deltaY < 0 ? 1.1 : 0.9;
+
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        cameraRef.current.zoomAt(x, y, factor);
+        forceRender();
+    }
+
+    return (
+        <svg
+            width={800}
+            height={600}
+            viewBox={cameraRef.current.viewBoxString}
+            style={{ border: "1px solid black" }}
+            onMouseMove={handleMouseMove}
+            onWheel={handleWheel}
+        >
+            <rect x={350} y={250} width={100} height={100} fill="steelblue" />
+        </svg>
+    );
 }
 
 export default Canvas;
